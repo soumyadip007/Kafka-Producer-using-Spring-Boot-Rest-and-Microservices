c4aaf24933ac6d34a3fb9e31a44b91e8874ed414
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-24 19:22:50 +0530

    Getters Setters -rm

--- producer/src/main/java/com/kafka/producer/entity/User.java
+++ producer/src/main/java/com/kafka/producer/entity/User.java
@@ -6,34 +6,5 @@ public class User {
 	private String name;
     private String dept;
     private int salary;
-    
-    public User(String name, String dept, int salary) {
-        this.name = name;
-        this.dept = dept;
-        this.salary = salary;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDept() {
-        return dept;
-    }
-
-    public void setDept(String dept) {
-        this.dept = dept;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
 }
