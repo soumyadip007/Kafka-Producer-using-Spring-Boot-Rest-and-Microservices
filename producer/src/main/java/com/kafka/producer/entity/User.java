89d910890948ad1f9c3be2f0751cdf0d304fd971
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-24 19:23:24 +0530

    Lombok dependencies added

--- producer/src/main/java/com/kafka/producer/entity/User.java
+++ producer/src/main/java/com/kafka/producer/entity/User.java
@@ -1,6 +1,16 @@
 package com.kafka.producer.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 	private String name;
