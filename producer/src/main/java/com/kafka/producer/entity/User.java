22b294e151424d23bd36cc8d0809a973321c584e
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 01:43:42 +0530

    200OK

--- producer/src/main/java/com/kafka/producer/entity/User.java
+++ producer/src/main/java/com/kafka/producer/entity/User.java
@@ -22,4 +22,33 @@ public class User {
         this.salary = salary;
     }
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public User() {
+		super();
+	}
+
+    
 }
